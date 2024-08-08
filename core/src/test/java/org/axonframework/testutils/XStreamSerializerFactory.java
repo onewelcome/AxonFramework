3c2f13e18a1447ef37a6e186c4a936bf3607de83
Adam Klinkosz <adam.klinkosz@onegini.com>
2024-08-08 09:37:35 +0200

    Fix tests due to restrictions introduced in newer XStream version

--- core/src/test/java/org/axonframework/testutils/XStreamSerializerFactory.java
+++ core/src/test/java/org/axonframework/testutils/XStreamSerializerFactory.java
@@ -6,11 +6,15 @@ import org.axonframework.serializer.xml.XStreamSerializer;
 
 public class XStreamSerializerFactory {
   public static XStreamSerializer create(Class<?>... permittedClasses) {
-    XStream xStream = new XStream();
-    xStream.registerConverter(new XStreamCopyOnWriteArraySetConverter(xStream.getMapper()));
-    xStream.addPermission(new ExplicitTypePermission(permittedClasses));
+    XStream xStream = createXStream(permittedClasses);
     return new XStreamSerializer(xStream);
   }
 
-
+  public static XStream createXStream(Class<?>... permittedClasses) {
+    XStream xStream = new XStream();
+    xStream.registerConverter(new XStreamCopyOnWriteArraySetConverter(xStream.getMapper()));
+    xStream.registerConverter(new XStreamUnmodifiableMapConverter(xStream.getMapper()));
+    xStream.addPermission(new ExplicitTypePermission(permittedClasses));
+    return xStream;
+  }
 }
